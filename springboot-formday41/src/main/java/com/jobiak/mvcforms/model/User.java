98275f04f30025a5febb8e236d3e5905d121e6d4
Chandini@22 <Chandinipriya20@gmail.com>
2022-05-17 10:38:39 +0530

    forms

---
+++ springboot-formday41/src/main/java/com/jobiak/mvcforms/model/User.java
@@ -0,0 +1,62 @@
+package com.jobiak.mvcforms.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="userdata")
+public class User {
+	@Id
+private Long userId;
+private String name;
+private String email;
+private Long mobile;
+
+public User() {
+	// TODO Auto-generated constructor stub
+}
+
+public User(String name, String email, Long mobile) {
+	super();
+	this.name = name;
+	this.email = email;
+	this.mobile = mobile;
+}
+
+
+public Long getUserId() {
+	return userId;
+}
+
+public void setUserId(Long userId) {
+	this.userId = userId;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public Long getMobile() {
+	return mobile;
+}
+
+public void setMobile(Long mobile) {
+	this.mobile = mobile;
+}
+
+
+
+}
